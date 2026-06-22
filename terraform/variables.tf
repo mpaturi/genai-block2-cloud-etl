675377a91fd04883c3d4d03d9333cf063d5ad606
Millie Paturi <millie.paturi@gmail.com>
2026-06-22 11:10:35 -0500

    Set default aws_region to us-east-2

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,7 +6,7 @@ variable "bucket_name" {
 variable "aws_region" {
   description = "AWS region for all resources"
   type        = string
-  default     = "us-east-1"
+  default     = "us-east-2"
 }
 
 variable "tags" {
