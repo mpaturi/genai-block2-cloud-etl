42c9634b627094ddbcaa1bbeab3ae88bfdae75d4
Millie Paturi <millie.paturi@gmail.com>
2026-06-23 19:51:25 -0500

    Add force_destroy and public access block to S3 bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,5 +1,15 @@
 resource "aws_s3_bucket" "pipeline" {
-  bucket = var.bucket_name
+  bucket        = var.bucket_name
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_public_access_block" "pipeline" {
+  bucket = aws_s3_bucket.pipeline.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "pipeline" {
